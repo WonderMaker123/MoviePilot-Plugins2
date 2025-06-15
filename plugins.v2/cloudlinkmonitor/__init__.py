53c3d95d48074e68c6bf908c47bf55373c1a5336
Xu Feng <98521813+WonderMaker123@users.noreply.github.com>
2025-06-15 11:36:08 +0800

    Update __init__.py

--- plugins.v2/cloudlinkmonitor/__init__.py
+++ plugins.v2/cloudlinkmonitor/__init__.py
@@ -64,9 +64,9 @@ class CloudLinkMonitor(_PluginBase):
     # 插件图标
     plugin_icon = "Linkease_A.png"
     # 插件版本
-    plugin_version = "2.6.0"
+    plugin_version = "2.6.2"
     # 插件作者
-    plugin_author = "thsrite"
+    plugin_author = "wonder"
     # 作者主页
     author_url = "https://github.com/thsrite"
     # 插件配置项ID前缀
